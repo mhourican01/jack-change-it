5563c165dd9dba6527bea7ad9fd44d6028d6932d
Michael Hourican <mhourican01@qub.ac.uk>
2021-05-06 15:07:57 +0100

    Normalised data casting

--- PlayerManager.py
+++ PlayerManager.py
@@ -3,20 +3,20 @@ from Player import Player
 # Asks user to input number of players, and creates as many player objects
 def initialisePlayers(players):
 
-    noOfPlayers = input("How many players are there? ")
+    noOfPlayers = int(input("How many players are there? "))
     print()
     
     try:
-        if int(noOfPlayers) < 2:
+        if noOfPlayers < 2:
             print("That's too few players! Please enter a number between 2-6.")
             initialisePlayers()
-        elif int(noOfPlayers) > 6:
+        elif noOfPlayers > 6:
             print("That's too many players! Please enter a number between 2-6.")
             initialisePlayers()
         else:
             # Creating player objects until user input, number of players, is reached
             # Player hand set to null until cards are dealt
-            for n in range(1, (int(noOfPlayers) + 1)):
+            for n in range(1, noOfPlayers + 1):
                 p = Player(n, None)
                 players.append(p)
     except ValueError:
